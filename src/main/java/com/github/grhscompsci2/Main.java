c530b8193dfbd664984f8bac1c4efa7d834d482c
Jeffrey Morris <ffejsirrom.seb@gmail.com>
2021-08-28 18:42:17 -0500

    Switch to Main

---
+++ src/main/java/com/github/grhscompsci2/Main.java
@@ -0,0 +1,12 @@
+package com.github.grhscompsci2;
+
+import com.github.grhscompsci2.engine.Window;
+
+public class Main {
+	public static void main(String[] args) {
+		Window window = Window.get();
+		window.run();
+
+	}
+
+}
