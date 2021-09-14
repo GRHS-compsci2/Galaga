648728855b6a8e6817f40fc6128e91ec731fba54
Astroid8 <astroid14502@gmail.com>
2021-09-14 13:31:51 -0500

    Create LoadingScreen.java

---
+++ core/src/com/github/grhscompsci2/galaga/LoadingScreen.java
@@ -0,0 +1,54 @@
+
+
+import com.badlogic.gdx.Screen;
+
+public class LoadingScreen implements Screen {
+    private Box2DTutorial parent; 
+
+
+public LoadingScreen(Box2DTutorial box2dTutorial){
+	parent = box2dTutorial;     
+}
+    @Override
+    public void show() {
+        
+        
+    }
+
+    @Override
+    public void render(float delta) {
+        
+        
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        
+        
+    }
+
+    @Override
+    public void pause() {
+        
+        
+    }
+
+    @Override
+    public void resume() {
+        
+        
+    }
+
+    @Override
+    public void hide() {
+        
+        
+    }
+
+    @Override
+    public void dispose() {
+        
+        
+    }
+    
+}
