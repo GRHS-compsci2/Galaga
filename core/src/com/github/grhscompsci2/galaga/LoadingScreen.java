c7706c400cb63ca22661ed1c8ea8a0b76c22b159
Rick Stover <stovri@grisd.net>
2021-09-15 09:00:36 -0500

    Fixed name of the game from Box2DTutorial to MyGdxGame

--- core/src/com/github/grhscompsci2/galaga/LoadingScreen.java
+++ core/src/com/github/grhscompsci2/galaga/LoadingScreen.java
@@ -1,54 +1,47 @@
-
+package com.github.grhscompsci2.galaga;
 
 import com.badlogic.gdx.Screen;
 
 public class LoadingScreen implements Screen {
-    private Box2DTutorial parent; 
+    private MyGdxGame parent;
 
+    public LoadingScreen(MyGdxGame game) {
+        parent = game;
+    }
 
-public LoadingScreen(Box2DTutorial box2dTutorial){
-	parent = box2dTutorial;     
-}
     @Override
     public void show() {
-        
-        
+
     }
 
     @Override
     public void render(float delta) {
-        
-        
+
     }
 
     @Override
     public void resize(int width, int height) {
-        
-        
+
     }
 
     @Override
     public void pause() {
-        
-        
+
     }
 
     @Override
     public void resume() {
-        
-        
+
     }
 
     @Override
     public void hide() {
-        
-        
+
     }
 
     @Override
     public void dispose() {
-        
-        
+
     }
-    
+
 }
