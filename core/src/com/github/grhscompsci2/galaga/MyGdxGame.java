a761fa0a8764bba8081b6b550bab29bded175745
Titan225 <cmckenzie@glenrosetigers.info>
2021-09-16 13:12:05 -0500

    commit

--- core/src/com/github/grhscompsci2/galaga/MyGdxGame.java
+++ core/src/com/github/grhscompsci2/galaga/MyGdxGame.java
@@ -1,13 +1,23 @@
 package com.github.grhscompsci2.galaga;
 
 import com.badlogic.gdx.ApplicationAdapter;
+<<<<<<< Updated upstream
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
+=======
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+>>>>>>> Stashed changes
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.badlogic.gdx.utils.ScreenUtils;
 
+<<<<<<< Updated upstream
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	ShapeRenderer shaper;
@@ -78,9 +88,43 @@ public class MyGdxGame extends ApplicationAdapter {
 		shaper.end();
 	}
 
+=======
+public class MyGdxGame extends ScreenAdapter {
+
+	private SpriteBatch batch  = new SpriteBatch();
+	private Texture img;
+	private Sprite sprite;
+
+	Assets myAssets = new Assets();
+	
+	
+	@Override
+	public void show () {
+		img = new Texture(myAssets.getImage());
+		sprite = new Sprite(img);
+		sprite.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+	}
+
+	@Override
+	public void render (float delta) {
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		//ScreenUtils.clear(1, 0, 0, 1);
+		batch.begin();
+		sprite.draw(batch);
+		batch.end();
+	}
+
+	@Override
+	public void hide(){
+		dispose();
+	}
+	
+>>>>>>> Stashed changes
 	@Override
 	public void dispose() {
 		batch.dispose();
 		shaper.dispose();
 	}
+
+	
 }
--- desktop/src/com/github/grhscompsci2/galaga/desktop/DesktopLauncher.java
+++ desktop/src/com/github/grhscompsci2/galaga/desktop/DesktopLauncher.java
@@ -2,12 +2,17 @@ package com.github.grhscompsci2.galaga.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+<<<<<<< Updated upstream
 import com.badlogic.gdx.graphics.Color;
+=======
+import com.github.grhscompsci2.galaga.MyAssetManager;
+>>>>>>> Stashed changes
 import com.github.grhscompsci2.galaga.MyGdxGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+<<<<<<< Updated upstream
 		config.backgroundFPS = 10;
 		config.foregroundFPS = 30;
 		config.height = 244;
@@ -17,5 +22,8 @@ public class DesktopLauncher {
 		config.vSyncEnabled = true;
 		config.width = 288;
 		new LwjglApplication(new MyGdxGame(), config);
+=======
+		new LwjglApplication(new MyAssetManager(), config);
+>>>>>>> Stashed changes
 	}
 }
