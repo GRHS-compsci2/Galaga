cd9b73ee77cf8732587e50d660efa5da64921915
Rick Stover <31324442+stovri@users.noreply.github.com>
2021-10-01 16:11:24 -0500

    Trying something new

--- core/src/com/github/grhscompsci2/galaga/MyGdxGame.java
+++ core/src/com/github/grhscompsci2/galaga/MyGdxGame.java
@@ -1,46 +1,31 @@
 package com.github.grhscompsci2.galaga;
 
-import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
 
-public class MyGdxGame extends Game{
-    private static ArcadeScreen arcadeScreen;
-    private static PreferencesScreen preferencesScreen;
-    private static LoadingScreen loadingScreen;
-
-	public static enum ScreenType{
-		Arcade,
-		Preferences,
-		Loading
-	}
-
-	public Screen getScreenType(ScreenType screenType){
-		switch(screenType){
-			case Arcade:
-				return arcadeScreen;
-			case Preferences:
-				return preferencesScreen;
-			case Loading:
-				return loadingScreen;
-			default:
-				return arcadeScreen;
-		}
-
+public class MyGdxGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
-	public void create(){
-        arcadeScreen=new ArcadeScreen(this);
-        preferencesScreen=new PreferencesScreen(this);
-        loadingScreen=new LoadingScreen(this);
-		setScreen(arcadeScreen);
+	public void render () {
+		ScreenUtils.clear(1, 0, 0, 1);
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
 	}
-
+	
 	@Override
-	public void dispose(){
-		arcadeScreen.dispose();
-        preferencesScreen.dispose();
-        loadingScreen.dispose();
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
 	}
-
 }
