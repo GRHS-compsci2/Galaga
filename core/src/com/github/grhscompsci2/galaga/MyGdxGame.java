5bdb4e605b1f1b3823d995d1d2dd9983356da52a
Rick Stover <31324442+stovri@users.noreply.github.com>
2021-10-13 12:27:37 -0500

    Cleaned up some of the files so it will be easier to complete the tasks. The utility class was created by Peyton.

---
+++ core/src/com/github/grhscompsci2/galaga/MyGdxGame.java
@@ -0,0 +1,52 @@
+package com.github.grhscompsci2.galaga;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
+import com.github.grhscompsci2.galaga.screens.ArcadeScreen;
+import com.github.grhscompsci2.galaga.screens.LoadingScreen;
+import com.github.grhscompsci2.galaga.screens.MenuScreen;
+import com.github.grhscompsci2.galaga.screens.PreferencesScreen;
+
+public class MyGdxGame extends Game{
+    private static ArcadeScreen arcadeScreen;
+    private static PreferencesScreen preferencesScreen;
+    private static LoadingScreen loadingScreen;
+	private static MenuScreen menuScreen;
+
+	public static enum ScreenType{
+		Arcade,
+		Preferences,
+		Loading
+	}
+
+	public Screen getScreenType(ScreenType screenType){
+		switch(screenType){
+			case Arcade:
+				return arcadeScreen;
+			case Preferences:
+				return preferencesScreen;
+			case Loading:
+				return loadingScreen;
+			default:
+				return menuScreen;
+		}
+
+	}
+
+	@Override
+	public void create(){
+        arcadeScreen=new ArcadeScreen(this);
+        preferencesScreen=new PreferencesScreen(this);
+        loadingScreen=new LoadingScreen(this);
+		menuScreen=new MenuScreen(this);
+		setScreen(menuScreen);
+	}
+
+	@Override
+	public void dispose(){
+		arcadeScreen.dispose();
+        preferencesScreen.dispose();
+        loadingScreen.dispose();
+	}
+
+}
---
+++ core/src/com/github/grhscompsci2/galaga/screens/LoadingScreen.java
@@ -0,0 +1,14 @@
+package com.github.grhscompsci2.galaga.screens;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.ScreenAdapter;
+import com.github.grhscompsci2.galaga.MyGdxGame;
+
+public class LoadingScreen extends ScreenAdapter {
+    private MyGdxGame parent;
+
+    public LoadingScreen(MyGdxGame myGdxGame) {
+        parent = myGdxGame;
+    }
+
+}
