67a568ed9129c563d1c5ad15c4aa2f99cc13f32d
Jeffrey Morris <ffejsirrom.seb@gmail.com>
2021-10-15 13:10:42 -0500

    fixed naming.

--- core/src/com/github/grhscompsci2/galaga/components/Translationcomponent.java
+++ core/src/com/github/grhscompsci2/galaga/components/Translationcomponent.java
@@ -3,7 +3,7 @@ package com.github.grhscompsci2.galaga.components;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.ashley.core.Component;
-public class Translationcomponent implements Component {
+public class TranslationComponent implements Component {
     public final Vector3 position = new Vector3();
     public final Vector2 scale = new Vector2(1.0f,1.0f);
     public float rotation= 0.0f;
