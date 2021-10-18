cb704c15cc300be5cdba55d69fcdca3d411789c4
Rick Stover <31324442+stovri@users.noreply.github.com>
2021-10-18 08:54:40 -0500

    Small fixes trying to get engine to work

--- core/src/com/github/grhscompsci2/galaga/entities/PlayerEntity.java
+++ core/src/com/github/grhscompsci2/galaga/entities/PlayerEntity.java
@@ -1,32 +1,11 @@
 package com.github.grhscompsci2.galaga.entities;
 
-<<<<<<< HEAD
-=======
 import com.badlogic.ashley.core.Entity;
 import com.github.grhscompsci2.galaga.Utility;
->>>>>>> a4bf55d (Got the groundwork for the RenderingSystem)
 import com.github.grhscompsci2.galaga.components.InputComponent;
 import com.github.grhscompsci2.galaga.components.TextureComponent;
 import com.github.grhscompsci2.galaga.components.TranslationComponent;
 
-<<<<<<< HEAD
-public class PlayerEntity {
-    TranslationComponent translationComponent;
-    InputComponent inputComponent;
-    TextureComponent textureComponent;
-
-    /**
-     * @param translationComponent
-     * @param inputComponent
-     * @param textureComponent
-     */
-    public PlayerEntity() {
-        this.translationComponent = new TranslationComponent();
-        this.inputComponent = new InputComponent();
-        this.textureComponent = new TextureComponent();
-    }
-
-=======
 public class PlayerEntity extends Entity {
     public PlayerEntity() {
         TextureComponent tex=new TextureComponent();
@@ -39,5 +18,4 @@ public class PlayerEntity extends Entity {
         TextureComponent tex=getComponent(TextureComponent.class);
         tex.region=Utility.getTextureRegionAsset("player1");
     }
->>>>>>> a4bf55d (Got the groundwork for the RenderingSystem)
 }
