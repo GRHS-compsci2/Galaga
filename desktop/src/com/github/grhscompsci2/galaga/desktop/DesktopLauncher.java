e5fbdf724a2681ad4d8f6187b78a74978378c767
Rick Stover <31324442+stovri@users.noreply.github.com>
2021-10-19 09:59:06 -0500

    Starter viewport

--- desktop/src/com/github/grhscompsci2/galaga/desktop/DesktopLauncher.java
+++ desktop/src/com/github/grhscompsci2/galaga/desktop/DesktopLauncher.java
@@ -11,7 +11,7 @@ public class DesktopLauncher {
 		config.foregroundFPS = 30;
 		config.height = 244;
 		config.initialBackgroundColor = Color.BLACK;
-		config.resizable = false;
+		config.resizable = true;
 		config.title = "Galaga";
 		config.vSyncEnabled = true;
 		config.width = 288;
