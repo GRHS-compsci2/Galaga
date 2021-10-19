8a7401618a65070189c15a14c574340041e2f8bc
Rick Stover <31324442+stovri@users.noreply.github.com>
2021-10-19 09:59:06 -0500

    Starter viewport

--- desktop/src/com/github/grhscompsci2/galaga/desktop/DesktopLauncher.java
+++ desktop/src/com/github/grhscompsci2/galaga/desktop/DesktopLauncher.java
@@ -7,6 +7,14 @@ import com.github.grhscompsci2.galaga.MyGdxGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.backgroundFPS = 10;
+		config.foregroundFPS = 30;
+		config.height = 244;
+		config.initialBackgroundColor = Color.BLACK;
+		config.resizable = true;
+		config.title = "Galaga";
+		config.vSyncEnabled = true;
+		config.width = 288;
 		new LwjglApplication(new MyGdxGame(), config);
 	}
 }
