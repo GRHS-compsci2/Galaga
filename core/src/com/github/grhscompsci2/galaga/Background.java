8f544d8e99239fb2d32633276bde14028c571360
Rick Stover <31324442+stovri@users.noreply.github.com>
2021-10-21 08:50:43 -0500

    Cleaned up import warnings and notes

--- core/src/com/github/grhscompsci2/galaga/Background.java
+++ core/src/com/github/grhscompsci2/galaga/Background.java
@@ -38,7 +38,7 @@ public class Background implements Screen {
 
     @Override
     public void show() {
-        // TODO Auto-generated method stub
+       
 
     }
 
@@ -73,31 +73,31 @@ public class Background implements Screen {
 
     @Override
     public void resize(int width, int height) {
-        // TODO Auto-generated method stub
+      
 
     }
 
     @Override
     public void pause() {
-        // TODO Auto-generated method stub
+       
 
     }
 
     @Override
     public void resume() {
-        // TODO Auto-generated method stub
+   
 
     }
 
     @Override
     public void hide() {
-        // TODO Auto-generated method stub
+        
 
     }
 
     @Override
     public void dispose() {
-        // TODO Auto-generated method stub
+       
 
     }
 
