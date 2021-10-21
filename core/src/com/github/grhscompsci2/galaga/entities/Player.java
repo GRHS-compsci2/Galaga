ec2d78084538213340fbd3d23f3b0c45380e0be3
Rick Stover <31324442+stovri@users.noreply.github.com>
2021-10-21 08:14:09 -0500

    Fixed file location issue

---
+++ core/src/com/github/grhscompsci2/galaga/entities/Player.java
@@ -0,0 +1,20 @@
+package com.github.grhscompsci2.galaga.entities;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Player {
+    Texture sprite; // new Texture("Galaga"); requires spritemap
+    int x;
+    int y;
+
+    public Player() {
+        sprite = new Texture("images/rawSprites/playerShip1.png");
+        x = 100;
+        y = 100;
+    }
+
+    public void draw(SpriteBatch batch) {
+        batch.draw(sprite, (float)x,(float)y);
+    }
+}
