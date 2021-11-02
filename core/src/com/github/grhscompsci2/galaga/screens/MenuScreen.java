28af1c287189cd0942f469c9461cfc008fb4da66
Jeffrey Morris <ffejsirrom.seb@gmail.com>
2021-11-02 13:38:06 -0500

    add sfx

--- core/src/com/github/grhscompsci2/galaga/screens/MenuScreen.java
+++ core/src/com/github/grhscompsci2/galaga/screens/MenuScreen.java
@@ -1,6 +1,5 @@
 package com.github.grhscompsci2.galaga.screens;
 
-import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.audio.Music;
@@ -13,38 +12,37 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
-import com.github.grhscompsci2.galaga.Background;
 import com.github.grhscompsci2.galaga.MyGdxGame;
-import com.github.grhscompsci2.galaga.Utility;
 import com.github.grhscompsci2.galaga.MyGdxGame.ScreenType;
+import com.github.grhscompsci2.galaga.Utility;
 
 public class MenuScreen extends ScreenAdapter {
 
     private Stage _stage;
     private SpriteBatch batch;
     private MyGdxGame parent;
-     private Music thememusic;
-    
+    private Music themeMusic;
+
     public MenuScreen(MyGdxGame game) {
-        parent=game;
-        batch=new SpriteBatch();
+        parent = game;
+        batch = new SpriteBatch();
         // Creation Table
         // Initialize Stage and Table for later use
-        Skin skin=Utility.STATUSUI_SKIN;
+        Skin skin = Utility.STATUSUI_SKIN;
         _stage = new Stage();
         Table table = new Table();
         table.setFillParent(true);
-        thememusic= Utility.getMusicAsset(Utility.themeMusic);
-       
+        themeMusic = Utility.getMusicAsset(Utility.themeMusic);
+
         // It is my job to import a image of the button i want to use
         // I could prob utilize my own class for that because
         // I can't use desktop or else it won't work for everbody
 
         /**
-         * We made a skin, then set it up in the utility class. The labels and text buttons,
-         * as well as a checkbox and slider were created there.
+         * We made a skin, then set it up in the utility class. The labels and text
+         * buttons, as well as a checkbox and slider were created there.
          */
-        Label title = new Label("GALAGA",skin);
+        Label title = new Label("GALAGA", skin);
         TextButton loadGameButton = new TextButton("Start", skin);
         TextButton prefrenceButton = new TextButton("Settings", skin);
         TextButton exitButton = new TextButton("Exit", skin);
@@ -52,9 +50,9 @@ public class MenuScreen extends ScreenAdapter {
         // Figure out what .spaceBottom does pleasse
         // I want to understand why I am doing this
         /**
-         * .spaceBottom is a method that tells the table how much space to put below the text.
-         * For instance, here we are inserting 75 pixels under the title to give some seperation 
-         * between it and the menu options.
+         * .spaceBottom is a method that tells the table how much space to put below the
+         * text. For instance, here we are inserting 75 pixels under the title to give
+         * some seperation between it and the menu options.
          */
         // Button Locations
         table.add(title).spaceBottom(75).row();
@@ -68,22 +66,21 @@ public class MenuScreen extends ScreenAdapter {
 
     }
 
-
     @Override
     public void render(float delta) {
-        
+
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         Utility.background.render(delta);
         batch.begin();
-        //_stage.act(delta);
+        // _stage.act(delta);
         _stage.draw();
         batch.end();
     }
 
     @Override
     public void dispose() {
-        
+
         _stage.dispose();
         batch.dispose();
     }
@@ -91,16 +88,15 @@ public class MenuScreen extends ScreenAdapter {
     @Override
     public void show() {
         Gdx.input.setInputProcessor(_stage);
-        thememusic.play();
-        Skin skin=Utility.STATUSUI_SKIN;
+        themeMusic.play();
+        Skin skin = Utility.STATUSUI_SKIN;
         Table table = new Table();
         table.setFillParent(true);
 
-        Label title = new Label("GALAGA",skin);
+        Label title = new Label("GALAGA", skin);
         TextButton loadGameButton = new TextButton("Start", skin);
         TextButton prefrenceButton = new TextButton("Settings", skin);
         TextButton exitButton = new TextButton("Exit", skin);
-
 
         table.add(title).spaceBottom(75).row();
         table.add(loadGameButton).spaceBottom(10).row();
@@ -110,28 +106,26 @@ public class MenuScreen extends ScreenAdapter {
         _stage.addActor(table);
 
         exitButton.addListener(new ChangeListener() {
-			@Override
-			public void changed(ChangeEvent event, Actor actor) {
-				Gdx.app.exit();				
-			}
-		});
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                Gdx.app.exit();
+            }
+        });
 
         loadGameButton.addListener(new ChangeListener() {
-			@Override
-			public void changed(ChangeEvent event, Actor actor) {
-				parent.setScreen(parent.getScreenType(ScreenType.Arcade));				
-			}
-		});
-
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                parent.setScreen(parent.getScreenType(ScreenType.Arcade));
+            }
+        });
 
         prefrenceButton.addListener(new ChangeListener() {
 
             @Override
-			public void changed(ChangeEvent event, Actor actor) {
-				parent.setScreen(parent.getScreenType(ScreenType.Preferences));				
-			}
-		});
-        
+            public void changed(ChangeEvent event, Actor actor) {
+                parent.setScreen(parent.getScreenType(ScreenType.Preferences));
+            }
+        });
 
     }
 
