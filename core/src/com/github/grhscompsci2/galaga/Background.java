699eb0ad2bc187bd39049d8fd496adb8ce2e5e0b
Rick Stover <31324442+stovri@users.noreply.github.com>
2021-12-13 13:41:40 -0600

    Made it possible to pause stars

--- core/src/com/github/grhscompsci2/galaga/Background.java
+++ core/src/com/github/grhscompsci2/galaga/Background.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 
 public class Background implements Screen {
+    private static final String TAG = Background.class.getSimpleName();
     ShapeRenderer shaper;
     int width;
     int height;
@@ -14,6 +15,8 @@ public class Background implements Screen {
     int starX[];
     int starY[];
     int bgFrameCounter = 0;
+    float accel = 0;
+    final private float ACCEL_INC=0.001f;
 
     public Background() {
         shaper = new ShapeRenderer();
@@ -38,67 +41,83 @@ public class Background implements Screen {
 
     @Override
     public void show() {
-       
 
     }
 
-    @Override
-    public void render(float delta) {
+    public void render(float delta, boolean paused) {
         shaper.begin(ShapeType.Filled);
 
-		// draw each star
-		for (int i = 0; i < numStars; i++) {
+        // draw each star
+        for (int i = 0; i < numStars; i++) {
 
-			// choose either bright or dim
-			if (Math.round(Math.random()) == 1) {
-				shaper.setColor(Color.GRAY);
-			} else {
-				shaper.setColor(Color.DARK_GRAY);
-			}
+            // choose either bright or dim
+            if (Math.round(Math.random()) == 1) {
+                shaper.setColor(Color.GRAY);
+            } else {
+                shaper.setColor(Color.DARK_GRAY);
+            }
 
-			// draw actual star.
-			shaper.rect(starX[i], starY[i], 2, 2);
+            // draw actual star.
+            shaper.rect(starX[i], starY[i], 2, 2);
 
-			// move star down random amounts.
-			starY[i] -= (int) (Math.random() * (5 - 3) + 3);
-			// move star to top & random x if below screen.
-			if (starY[i] < 1) {
-				starY[i] = height;
-				starX[i] = (int) (Math.random() * (width - 1) + 1);
-			}
-		}
+            // move star down random amounts.
+            if (!paused) {
+                starY[i] -= (int) ((Math.random() * (5 - 3) + 3) * accel);
+                if (accel < 1) {
+                    accel += ACCEL_INC;
+                } else {
+                    accel = 1;
+                }
+            } else {
+                starY[i] -= (int) ((Math.random() * (5 - 3) + 3) * accel);
+                if (accel > 0) {
+                    accel -= ACCEL_INC;
+                } else {
+                    accel = 0;
+                }
+            }
+            if (accel > 0 && accel < 1) {
+                Gdx.app.debug(TAG, "Accel:" + accel);
+            }
+            // move star to top & random x if below screen.
+            if (starY[i] < 1) {
+                starY[i] = height;
+                starX[i] = (int) (Math.random() * (width - 1) + 1);
+            }
+        }
 
-		shaper.end();
+        shaper.end();
     }
 
     @Override
     public void resize(int width, int height) {
-      
 
     }
 
     @Override
     public void pause() {
-       
 
     }
 
     @Override
     public void resume() {
-   
 
     }
 
     @Override
     public void hide() {
-        
 
     }
 
     @Override
     public void dispose() {
-       
 
+    }
+
+    @Override
+    public void render(float delta) {
+        // TODO Auto-generated method stub
+        render(delta, false);
     }
 
 }
