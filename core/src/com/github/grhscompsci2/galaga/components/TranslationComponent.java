08544f3b6d0b8ca18c47ac33c2e75d2e33c23273
Rick Stover <31324442+stovri@users.noreply.github.com>
2022-01-07 09:08:02 -0600

    Removed static from position

--- core/src/com/github/grhscompsci2/galaga/components/TranslationComponent.java
+++ core/src/com/github/grhscompsci2/galaga/components/TranslationComponent.java
@@ -4,21 +4,21 @@ import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.ashley.core.Component;
 public class TranslationComponent implements Component {
-    public static Vector3 position=new Vector3();
+    public Vector3 position=new Vector3();
     public Vector2 scale = new Vector2(1.0f,1.0f);
     public float rotation= 0.0f;
     public boolean isHidden=false;
     public void setPosition(float x, float y) {
         position.set(x, y, 0.0f);
     }
-    public static Vector3 getPosition() {
+    public Vector3 getPosition() {
         return position;
     }
-    public void setPosition(Vector3 position2) {
+    /*public void setPosition(Vector3 position2) {
        float i = 0.0f;
        float j = 0.0f;
-       Vector3 fill = new Vector3(i,j,0.0f);
-       fill = position2;
-        position.set(i,j,0.0f);
-    }
+       //Vector3 fill = new Vector3(i,j,0.0f);
+       //fill = position2;
+        //position.set(i,j,0.0f);
+    }*/
 }
