37130e3ad975bcc41d128150c13b01956afc750c
stovri <stovri@grisd.net>
2022-04-23 12:19:07 -0500

    Fixed the idle state transition

--- core/src/com/github/grhscompsci2/galaga/systems/EnemySystem.java
+++ core/src/com/github/grhscompsci2/galaga/systems/EnemySystem.java
@@ -3,6 +3,8 @@ package com.github.grhscompsci2.galaga.systems;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.systems.IteratingSystem;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Vector2;
 import com.github.grhscompsci2.galaga.ai.SteeringPresets;
 import com.github.grhscompsci2.galaga.components.EnemyComponent;
 import com.github.grhscompsci2.galaga.components.Mapper;
@@ -12,6 +14,9 @@ import com.github.grhscompsci2.galaga.components.SteeringComponent.SteeringState
 
 public class EnemySystem extends IteratingSystem {
     private static String TAG = EnemySystem.class.getSimpleName();
+    private static Vector2 idler = new Vector2();
+    private static boolean goingLeft = false;
+    private static float idleTime = 0;
 
     @SuppressWarnings("unchecked")
     public EnemySystem() {
@@ -23,14 +28,17 @@ public class EnemySystem extends IteratingSystem {
     protected void processEntity(Entity entity, float deltaTime) {
         // grab the specific enemy and body components for the entity
         StateComponent stateComponent = Mapper.stateCom.get(entity);
-        // decide what to do based on the entity's state
+
+        updateFormation(deltaTime);
         switch (stateComponent.getState()) {
             case StateComponent.STATE_ENTRY:
                 entry(entity);
                 break;
+            case StateComponent.STATE_ENTRY_GO_HOME:
+                entryGoHome(entity);
+                break;
             case StateComponent.STATE_ENTRY_IDLE:
                 entryIdle(entity);
-                break;
         }
     }
 
@@ -45,21 +53,63 @@ public class EnemySystem extends IteratingSystem {
         }
         if (enemyComponent.areWeThereYet(steeringComponent.followPath.getArrivalTolerance(),
                 steeringComponent.getPosition())) {
-            stateComponent.set(StateComponent.STATE_ENTRY_IDLE);
+            stateComponent.set(StateComponent.STATE_ENTRY_GO_HOME);
             steeringComponent.currentMode = SteeringState.STOP;
         }
 
     }
 
+    private void entryGoHome(Entity entity) {
+        SteeringComponent steeringComponent = Mapper.sCom.get(entity);
+        EnemyComponent enemyComponent = Mapper.enemyCom.get(entity);
+        StateComponent stateComponent = Mapper.stateCom.get(entity);
+        // Go to the home position
+        // if (steeringComponent.currentMode != SteeringState.GO) {
+        steeringComponent
+                .setSteeringBehavior(
+                        SteeringPresets.goPoint(steeringComponent, enemyComponent.updateHome(idler), 0.5f));
+        enemyComponent.setPath(steeringComponent.followPath.getPath());
+        // steeringComponent.currentMode = SteeringState.GO;
+        // }
+        if (enemyComponent.areWeThereYet(steeringComponent.followPath.getArrivalTolerance(),
+                steeringComponent.getPosition())) {
+            steeringComponent.currentMode = SteeringState.STOP;
+            stateComponent.set(StateComponent.STATE_ENTRY_IDLE);
+        }
+    }
+
     private void entryIdle(Entity entity) {
         SteeringComponent steeringComponent = Mapper.sCom.get(entity);
         EnemyComponent enemyComponent = Mapper.enemyCom.get(entity);
-        // StateComponent stateComponent = Mapper.stateCom.get(entity);
         if (steeringComponent.currentMode != SteeringState.GO) {
-            steeringComponent
-                    .setSteeringBehavior(SteeringPresets.goPoint(steeringComponent, enemyComponent.getHome()));
+            steeringComponent.steeringBehavior = null;
             steeringComponent.currentMode = SteeringState.GO;
         }
-
+        steeringComponent.setPosition(enemyComponent.updateHome(idler));// .add(idler));
+        // }
     }
+
+    /**
+     * This method will update the position of the formation, so all enemies can
+     * move in sync
+     * 
+     * @param deltaTime the elapsed time
+     */
+    public void updateFormation(float deltaTime) {
+        if (deltaTime > 1)
+            deltaTime = 1;
+        idleTime += deltaTime;
+
+        float x = (idleTime) / 8;
+        if (goingLeft) {
+            x *= -1;
+        }
+        if (idleTime > 1) {
+            idleTime = 0;
+            goingLeft = !goingLeft;
+        }
+        idler.set(x, 0);
+        // Gdx.app.debug(TAG, "Idler: " + idler);
+    }
+
 }
